a3c2ac6b25347a03a445d8f172387b25cef9d0bb
DyukovNA <1ceptnpd@gmail.com>
2026-05-03 17:51:30 +0300

    S3 and p2p delete enabled

--- docker/tinode/entrypoint.sh
+++ docker/tinode/entrypoint.sh
@@ -148,4 +148,5 @@ fi
 args=("--config=${CONFIG}" "--static_data=$STATIC_DIR" "--cluster_self=$CLUSTER_SELF" "--pprof_url=$PPROF_URL")
 
 # Run the tinode server.
-./tinode "${args[@]}" 2>> /var/log/tinode.log
+# Logs go to stdout/stderr (captured by container runtime for kubectl logs)
+./tinode "${args[@]}"
